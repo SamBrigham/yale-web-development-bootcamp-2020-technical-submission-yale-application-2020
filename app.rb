d11229319090c2b4cb623a53972426951af03673
Samuel Brigha <sam.brigham@yale.edu>
2020-04-21 16:08:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ name = "Sam"
 age = 21
 favorite_food = "Nachos"
 height_in_inches = 72
+average_height = 60
 height_message = height_in_inches ? "You are taller than average" : "You are not taller than average"
 
 
